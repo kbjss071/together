e4668589ee2e4c2602078b6eeff2652ad2b927f4
Jin Kang <kbjss071@gmail.com>
2022-10-23 20:50:23 -0400

    initial commit for typedefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,54 @@
+const {gql} = require('apollo-server-express')
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String
+        password: String
+        post: [Post]
+        donation: [Donation]
+        comment: [Comment]
+    }
+    
+    type Post {
+        _id: ID!
+        title: String
+        content: String
+        user: User
+    }
+
+    type Donation {
+        _id: ID!
+        user: User
+        post: Post
+        amount: Float
+    }
+
+    type Comment {
+        _id: ID!
+        post: Post
+        content: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        user: User
+        posts: [Post]
+        comments: [Comment]
+        donations: [Donation]
+    }
+
+    type Mutation {
+        login(username: String!, password: String!): Auth
+        addUser(username: String!, password: String!): Auth
+        addPost(username: String!): Post
+        addComment(username: String!, post: ID!): Comment
+        addDonation(username: String!, post: ID!): Donation
+    }
+`
+
+module.exports = typeDefs
