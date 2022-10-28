d865d13201e4b4fe5a608591d00f6337464e152d
Jin Kang <kbjss071@gmail.com>
2022-10-28 15:40:49 -0400

    navbar

---
+++ client/src/components/Navbar/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Auth from '../../utils/utils';
+import { Link } from 'react-router-dom';
+
+function Nav(){
+    function showNavigation(){
+        if(Auth.loggedIn)
+    }
+}
